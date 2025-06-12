f20402cd94487df820e844bfdf5b789a4120a333
Camron Gonzalez <camrong520@gmail.com>
2025-06-12 16:05:37 -0400

    testing some stuff, added some notes

--- title-screen/src/lib.rs
+++ title-screen/src/lib.rs
@@ -1,5 +1,21 @@
 // This is where your main game loop code goes
 // The stuff in this block will run ~60x per sec
+
+//rust is an inverted y-axis engine
+//As y goes towards positive infinity, the position goes down the screen.
+
+
 turbo::go!({
-    text!("Hello, to the bueatiful turbo world!!");
+    text!(
+        "Hello, to the bueatiful turbo world!!",
+        x = 50,
+        y = 50,
+        font = "medium");
+    
+    rect!(
+        w = 20,
+        h = 20,
+        x = 40,
+        y = 60
+    );
 });
